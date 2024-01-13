c52ba856eadd7e6e2f98c2ee8bb9c7e0bf8d386b
boouee <104202645+boouee@users.noreply.github.com>
2024-01-13 16:10:33 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -25,7 +25,7 @@
       $result = json_decode(curl_exec($ch), true); 
       //var_dump(json_encode(curl_exec($ch)=>data));
       curl_close ($ch);  
-      var_dump($result["data"][6]);
+      var_dump($result["data"][6][3250]["value"]);
 
 
 ?> 
