bc531938ac1ef764603fad58870937e445a8e795
boouee <104202645+boouee@users.noreply.github.com>
2024-01-13 16:03:52 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -22,10 +22,10 @@
       curl_setopt($ch, CURLOPT_POST, 1);  
       curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));  
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  
-      $result = json_decode(curl_exec($ch)); 
-      var_dump(json_encode(curl_exec($ch)=>data));
+      $result = json_decode(curl_exec($ch)=>data); 
+      //var_dump(json_encode(curl_exec($ch)=>data));
       curl_close ($ch);  
-      //var_dump($result);
+      var_dump($result);
 
 
 ?> 
