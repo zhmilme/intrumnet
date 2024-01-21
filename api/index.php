959ad53a5165fcda6c09b289d14c8c725b3ee3ae
zhmilme <157293813+zhmilme@users.noreply.github.com>
2024-01-21 17:53:34 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -57,7 +57,7 @@
       curl_close ($ch);  
       //var_dump($json["data"]);
       //var_dump($encoded["data"]); 
-      var_dump($result["data"][$_GET["id"]]["fields"][3644]);
+      var_dump($result["data"][$_GET["id"]]["fields"][9502062]);
       var_dump($result["data"][$_GET["id"]]["fields"][2397]);
       //var_dump($result["data"][$_GET["id"]]["fields"][11]["value"]);
 ?> 
