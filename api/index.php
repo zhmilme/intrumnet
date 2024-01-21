29a6f32c1fcd489f1ae139ad18ed98e2d6b16e3e
zhmilme <157293813+zhmilme@users.noreply.github.com>
2024-01-21 17:10:10 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -33,8 +33,8 @@
       
       $post = array(  
         //'apikey' =>"11e7e2bc70df4edb0178ff274b5ac07a");  
-          'apikey' =>"11e7e2bc70df4edb0178ff274b5ac07a");  
-      $url='http://trial.intrumnet.com:81/sharedapi/worker/filter';  
+          'apikey' =>"1f64ddc5d1fb0679c5fd1221ee738685");  
+      $url='https://aires.astoria-tula.ru:81/sharedapi/worker/filter';  
   
       $params=array(  
                 'id'=>$_GET["id"]);    
@@ -42,7 +42,7 @@
 
       
       $post = array(  
-        'apikey' =>"11e7e2bc70df4edb0178ff274b5ac07a",  
+        'apikey' =>"1f64ddc5d1fb0679c5fd1221ee738685",  
         'params'=>$params  
       );  
           
