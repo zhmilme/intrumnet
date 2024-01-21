283d7d1fb684a19af7c0f6d2ee2aabb14e31ae30
zhmilme <157293813+zhmilme@users.noreply.github.com>
2024-01-21 17:18:38 +0300

    Update and rename index_hello.php to index.php

--- api/index.php
+++ api/index.php
@@ -4,8 +4,8 @@
       
       $post = array(  
         //'apikey' =>"11e7e2bc70df4edb0178ff274b5ac07a");  
-          'apikey' =>"11e7e2bc70df4edb0178ff274b5ac07a");  
-      $url='http://trial.intrumnet.com:81/sharedapi/worker/fields';  
+          'apikey' =>"1f64ddc5d1fb0679c5fd1221ee738685");  
+      $url='https://aires.astoria-tula.ru/sharedapi/worker/fields';  
   
       $params=array(  
                 'id'=>$_GET["id"]);    
@@ -13,7 +13,7 @@
 
       
       $post = array(  
-        'apikey' =>"11e7e2bc70df4edb0178ff274b5ac07a"  
+        'apikey' =>"1f64ddc5d1fb0679c5fd1221ee738685"  
         'params'=>$params  
       );  
           
