abc62839f71ce7196b58ea47dc15d250da7853aa
boouee <104202645+boouee@users.noreply.github.com>
2024-01-21 15:20:49 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -3,7 +3,8 @@
       //$url    = 'http://trial.intrumnet.com:81/sharedapi/worker/fields';  
       
       $post = array(  
-        'apikey' =>"11e7e2bc70df4edb0178ff274b5ac07a");  
+        //'apikey' =>"11e7e2bc70df4edb0178ff274b5ac07a");  
+          'apikey' =>"1f64ddc5d1fb0679c5fd1221ee738685");  
       $url='http://trial.intrumnet.com:81/sharedapi/worker/filter';  
   
       $params=array(  
