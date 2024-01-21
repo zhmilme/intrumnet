1410c4695a582da773a425f8c5489509963a87ed
boouee <104202645+boouee@users.noreply.github.com>
2024-01-21 16:32:55 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
           
       //$url    = 'http://trial.intrumnet.com:81/sharedapi/worker/fields';  
       echo ($_GET["id"]);
-      $post = array(  
+      /* $post = array(  
         //'apikey' =>"11e7e2bc70df4edb0178ff274b5ac07a");  
           'apikey' =>"11e7e2bc70df4edb0178ff274b5ac07a");  
       $url='https://trial.intrumnet.com:81/sharedapi/worker/filter';  
@@ -29,7 +29,34 @@
       //var_dump(json_encode(curl_exec($ch)=>data));
       curl_close ($ch);  
       var_dump($result);
-      var_dump($result["data"][$_GET["id"]]["fields"][3250]["value"]);
+      var_dump($result["data"][$_GET["id"]]["fields"][3250]["value"]);*/
+      
+      $post = array(  
+        //'apikey' =>"11e7e2bc70df4edb0178ff274b5ac07a");  
+          'apikey' =>"11e7e2bc70df4edb0178ff274b5ac07a");  
+      $url='http://trial.intrumnet.com:81/sharedapi/worker/fields';  
+  
+      $params=array(  
+                'id'=>$_GET["id"]);    
+                //'name'=>'Ivan');    
+
+      
+      $post = array(  
+        'apikey' =>"11e7e2bc70df4edb0178ff274b5ac07a"  
+        'params'=>$params  
+      );  
+          
+      $ch = curl_init();  
+      curl_setopt($ch, CURLOPT_URL, $url);  
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);  
+      curl_setopt($ch, CURLOPT_POST, 1);  
+      curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));  
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  
+      $result = json_decode(curl_exec($ch));  
+      //var_dump(json_encode(curl_exec($ch)=>data));
+      curl_close ($ch);  
+      var_dump($result);
+
 
 
 ?> 
