0b7fae0b3dec20dd26c4e8f6f6d997c9f7e62719
zhmilme <157293813+zhmilme@users.noreply.github.com>
2024-01-23 14:50:56 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
           
       //$url    = 'http://trial.intrumnet.com:81/sharedapi/worker/fields';  
       
-      $post = array(  
+     /* $post = array(  
         //'apikey' =>"11e7e2bc70df4edb0178ff274b5ac07a");  
           'apikey' =>"1f64ddc5d1fb0679c5fd1221ee738685");  
       $url='https://aires.astoria-tula.ru/sharedapi/worker/fields';  
@@ -27,7 +27,7 @@
       //var_dump(json_encode(curl_exec($ch)=>data));
       curl_close ($ch);  
       //var_dump($result);
-
+          */
       echo ("\n \n");
       $post = array(  
         //'apikey' =>"11e7e2bc70df4edb0178ff274b5ac07a");  
@@ -43,7 +43,7 @@
         'apikey' =>"1f64ddc5d1fb0679c5fd1221ee738685",  
         'params'=>$params  
       );  
-          
+                    
       $ch = curl_init();  
       curl_setopt($ch, CURLOPT_URL, $url);  
       curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);  
@@ -51,8 +51,8 @@
       curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));  
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  
       $result = json_decode(curl_exec($ch),true);  
-      $json = curl_exec($ch);
-      $encoded = json_encode(curl_exec($ch));
+      //$json = curl_exec($ch);
+      //$encoded = json_encode(curl_exec($ch));
       //var_dump(json_encode(curl_exec($ch)=>data));
       curl_close ($ch);  
       //var_dump($json["data"]);
