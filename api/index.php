48b1ece2c3ad921c02aef371f22180c07ed3800c
zhmilme <157293813+zhmilme@users.noreply.github.com>
2024-01-23 19:36:24 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -3,8 +3,8 @@
             //echo "Making request";
             $url='https://aires.astoria-tula.ru/sharedapi/worker/filter';  
             $post = array('apikey' =>"1f64ddc5d1fb0679c5fd1221ee738685");
-            $url='https://trial.intrumnet.com/sharedapi/worker/filter'; 
-            $post = array('apikey' =>"11e7e2bc70df4edb0178ff274b5ac07a");
+            //$url='https://trial.intrumnet.com/sharedapi/worker/filter'; 
+            //$post = array('apikey' =>"11e7e2bc70df4edb0178ff274b5ac07a");
             $ch = curl_init();  
             curl_setopt($ch, CURLOPT_URL, $url);  
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);  
